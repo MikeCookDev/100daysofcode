016bf2097a2b0addca477d96bff0b4530328fc75
Mike Cook <mikecookdev@gmail.com>
2018-10-30 22:19:23 -0700

    Add files via upload

---
+++ day-36/day_36.js
@@ -0,0 +1,13 @@
+alert("Welcome to day 36!!");
+
+var HappyHalloween = "Happy Halloween! ";
+	document.write(HappyHalloween);
+
+
+var Safe = "<br>Stay safe and have fun!";
+	document.write(Safe);
+
+document.write("<h1>Hello World!</h1>");
+
+
+
