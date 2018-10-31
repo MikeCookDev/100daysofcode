e47b331c06b3185b814ddc02e2f31c123bf3a5f9
Mike Cook <mikecookdev@gmail.com>
2018-10-31 14:33:13 -0700

    Add files via upload

---
+++ day-37/day_37.js
@@ -0,0 +1,103 @@
+//alert("Welcome to day 37!!");
+
+document.write("<h1>JavaScript Practice Page<br></h1>");
+
+var HappyHalloween = "Happy Halloween! ";
+	document.write(HappyHalloween);
+
+
+var Safe = "<br>Stay safe and have fun!";
+	document.write(Safe);
+
+var OctThirtyFirst = "<br>Today is October.31.2018!";
+	document.write(OctThirtyFirst);
+
+var sayHi = '<br>Hello world! \'I am learning JavaScript.\'<br>';
+	document.write(sayHi);
+
+var todayIsHalloween = true;
+var todayIsChristmas = false;
+
+document.write("<br>Lets try some math operators! <br>");
+
+var x=10+3;
+document.write(x);
+//outputs 13
+
+document.write("<br>")
+
+var x = 13;
+var y = x + 28 + 9 + 113 + 503;
+document.write(y);
+//Outputs 666
+
+document.write("<br>")
+
+var x=10*3;
+document.write(x);
+//outputs 30
+document.write("<br>")
+
+var x=10/5;
+document.write(x);
+//outputs 2
+document.write("<br>")
+
+var x=98%5;
+document.write(x);
+//outputs 3
+document.write("<br>")
+
+var x=0,y= 30;
+var x = ++y;
+document.write(x);
+//outputs 31
+document.write("<br>")
+
+var x=0,y= 30;
+var x = --y;
+document.write(x);
+//outputs 29
+document.write("<br>")
+
+var result = 20;
+result*=5;
+document.write(result);
+//outputs 100
+
+document.write("<br>");
+
+document.write("<br>Lets try some comparison operators! <br>");
+
+document.write("<br>");
+
+var num = 20 == 12;
+document.write(num);
+//num == 12 will return false
+
+document.write("<br>");
+
+var num=13 == 13;
+document.write(num);
+//num == 13 will return true
+
+document.write("<br>");
+
+document.write("<br>Lets try some Logical and Boolean Operators <br>");
+document.write("<br>");
+
+var Logic=(5>4)&&(9<18);
+	document.write(Logic);
+	//output returns true
+
+document.write("<br>");
+
+var string1 = "I am learning";
+var string2 = "JavaScript, slowly but surely!";
+	document.write(string1 + string2);
+
+document.write("<br>");
+
+var x = "50";
+var y = "100";
+document.write(x+y);
