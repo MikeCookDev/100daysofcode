55970e50da59fd19e390a968ea3f70a84a075eba
Mike Cook <mikecookdev@gmail.com>
2018-11-01 22:27:49 -0700

    Add files via upload

---
+++ day-38/day_38.js
@@ -0,0 +1,16 @@
+//functions
+
+//User defined functions
+
+function myFunction(){
+	alert("Calling a Function!");
+}
+
+myFunction();
+//Alerts"Calling a Function!"
+
+myFunction();
+//"Alert box!"
+
+
+
