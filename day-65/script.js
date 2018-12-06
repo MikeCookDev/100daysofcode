2d11de0feafdf3a1f55207aec40056bf567b89b8
Mike Cook <mikecookdev@gmail.com>
2018-12-05 22:37:28 -0800

    Add files via upload

---
+++ day-65/script.js
@@ -0,0 +1,11 @@
+var visitor = prompt('What is your name?');
+var message = 'Hello ' + visitor + " Welcome to the site! ";
+message += " We are so glad you stopped by! ";
+message += visitor;
+message += ". Please come again!";
+document.write(message);
+
+var firstName ="Mike";
+var lastName = "Cook";
+var fullName = firstName + lastName ;
+document.write(fullName);
