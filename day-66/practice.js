155bba27fe390ce9460c2ee5fde872e87250c9a0
Mike Cook <mikecookdev@gmail.com>
2018-12-06 23:03:28 -0800

    Add files via upload

---
+++ day-66/practice.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var fullName = firstName.toUpperCase() + " " + lastName.toUpperCase();
+var number = (fullName.length);
+alert("The string \"" + fullName + "\" is " + number + " characters long.");
---
+++ day-66/story_program.js
@@ -0,0 +1,10 @@
+"<h2>The [adjective] little [noun] [verb] the tree.</h2>"
+
+var describe =prompt("Please type in an adjective");
+var thing =prompt("Please type in a noun");
+var action =prompt("Please type in a verb");
+alert("All done, ready to read your story?");
+
+var story= "The " + describe + " little " + thing + " " + action + " the tree."
+
+document.write(story);
