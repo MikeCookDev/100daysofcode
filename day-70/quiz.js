79b7a8fb355420d8db41e4989fd824b71f746217
Mike Cook <mikecookdev@gmail.com>
2018-12-16 23:21:48 -0800

    Add files via upload

---
+++ day-70/quiz.js
@@ -0,0 +1,13 @@
+var answer = prompt('What programming language is the name of a gem?');
+if (answer.toUpperCase() === 'RUBY' ) {
+	document.write("<p>That's right!</p>");
+} else {
+	document.write ("<p> Sorry, that's wrong.</p>");
+}
+
+/*var answer = prompt('What is the best programming language?');
+if (answer === "JavaScript") {
+    alert("<p> You are correct</p>");
+}else {
+ 	alert("<p> JavaScript is the best language!"); 
+}*/
