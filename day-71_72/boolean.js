39f03b01e13adf78321efce1eb9493dd52b735a0
Mike Cook <mikecookdev@gmail.com>
2018-12-18 23:03:17 -0800

    Add files via upload

---
+++ day-71_72/boolean.js
@@ -0,0 +1,29 @@
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+if (parseInt(guess) === randomNumber ) {
+  correctGuess = true;
+} 
+if ( correctGuess ) {
+		document.write('<p>You guessed the number!</p>');
+} else {
+	document.write('<p>Sorry. The number was ' + randomNumber + '. </p>');
+}
+
+
+
+
+//More Practice
+
+/*
+var isAdmin = false;
+var isStudent = false;
+
+if ( isAdmin ) {
+    alert('Welcome administrator');
+} else if (isStudent) {
+    alert('Welcome student');
+}else {
+  alert("Who are you?");
+}
+*/
