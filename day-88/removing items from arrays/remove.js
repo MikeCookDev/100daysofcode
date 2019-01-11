e58e1b18a6a065002943657e886f25404217edfc
Mike Cook <mikecookdev@gmail.com>
2019-01-10 22:31:22 -0800

    Add files via upload

---
+++ day-88/removing items from arrays/remove.js
@@ -0,0 +1,27 @@
+var nums = [1,2,3,4,5]
+
+nums.length
+
+nums.push(6);
+
+nums
+
+nums.pop()
+
+nums
+
+nums.unshift(0);
+
+nums
+
+nums.shift();
+
+nums
+
+nums.shift();
+
+nums.shift();
+
+nums
+
+//DO this in the browser console
