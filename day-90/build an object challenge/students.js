d6e3eaaa061f4170e5f3dac324848315f43326ec
Mike Cook <mikecookdev@gmail.com>
2019-01-14 07:51:53 -0800

    Add files via upload

---
+++ day-90/build an object challenge/students.js
@@ -0,0 +1,34 @@
+var students = [
+	{
+	name: 'Tim',
+	track: 'iOS',
+	achievements: 613,
+	points:900, 
+	},
+	{
+	name: 'Eliza',
+	track: 'Web Design',
+	achievements: '175',
+	points: '587', 
+	},
+	{
+	name: 'Anna',
+	track: 'Front End Development',
+	achievements: '452',
+	points: '836', 
+	},
+	{
+	name: 'Jake',
+	track: 'JavaScript',
+	achievements: '6',
+	points: '589', 
+	},
+	{
+	name: 'Josh',
+	track: 'Android',
+	achievements: '274',
+	points:'795', 
+	},
+];
+
+
