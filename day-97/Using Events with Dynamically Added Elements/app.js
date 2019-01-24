8e54c64d01738c60df4356196265c41b722c7dd4
Mike Cook <mikecookdev@gmail.com>
2019-01-23 22:35:46 -0800

    Add files via upload

---
+++ day-97/Using Events with Dynamically Added Elements/app.js
@@ -0,0 +1,15 @@
+$('.spoiler').on('click', 'button', function(){
+	//Show the spoiler text
+ 	$('.spoiler span').show();
+    //Hide the "Reveal Spoiler" button
+    $('.spoiler button').hide();
+});
+
+//Create the "Reveal Spoiler" button
+const $button = $('<button>Reveal Spoiler</button>');
+//Append to web page
+$('.spoiler').append($button);
+//Hide the spoiler text
+$('.spoiler span').hide();
+
+
