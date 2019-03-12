8d527bd37348fe655c94d961b54550010f6a51b5
Mike Cook <mikecookdev@gmail.com>
2019-03-12 07:48:45 -0700

    Add files via upload

---
+++ day-105/ajax and apis/weather app practice/weather.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
+  var data = {
+    q : "Claremont,CA",
+    units : "metric"
+  };
+  function showWeather(weatherReport){
+  $("#temperature").text(weatherReport.main.temp);
+  };
+  $.getJSON(weatherAPI, data, showWeather);
+});
